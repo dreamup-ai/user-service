eeac5b1a490a81307860bb6150570f9c816ad8f9
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2023-04-12 11:40:33 -0400

    Getting started

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { DatabaseTable } from "db-dynamo";
+import { QueueManager } from "queue-sqs";
+import Fastify, { FastifyInstance } from "fastify";
--- test/env.ts
+++ test/env.ts
@@ -1,4 +1,5 @@
 Object.assign(process.env, {
+  AWS_REGION: "us-east-1",
   DYNAMODB_ENDPOINT: "http://localhost:8000",
   SQS_ENDPOINT: "http://localhost:4566/",
 });
